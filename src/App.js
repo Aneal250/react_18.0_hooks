79439958abc2f5924afd24bf881eee8c21dea28b
anaele nnaemeka <anaelennaemeka@gmail.com>
2023-01-24 13:02:52 +0100

    useDebugValue Implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import { useDebugValue } from "react";
+import { useOnlineStatus } from "./components/useDebugValue";
+
+function StatusBar() {
+  const isOnline = useOnlineStatus();
+  return <h1>{isOnline ? "✅ Online" : "❌ Disconnected"}</h1>;
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to React 18.0 Hooks</h1>
+      <h2>useDebugValue Hooks</h2>
+      <StatusBar />
+      <p>
+        useDebugValue is a React Hook that lets you add a label to a custom Hook
+        in React DevTools.
+      </p>
     </div>
   );
 }
